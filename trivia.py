857e7b57aa2dda1c54b749713df9fae45124a0e0
Samuel Karjallo <karjallo@disroot.org>
2026-03-11 17:25:10 -0300

    corregimos espacios y aceptamos respuestas validas a pesar de estar en mayusculas

--- trivia.py
+++ trivia.py
@@ -3,17 +3,17 @@ puntaje = 0
 
 print(f'bienvenido {nombre_usuario}')
 
-pregunta1 = input('cual es el nombre del coach de cabello largo?')
-if pregunta1 == 'olaf':
+pregunta1 = input('cual es el nombre del coach de cabello largo?: ')
+if pregunta1.lower() == 'olaf':
     puntaje = puntaje + 1
-pregunta2 = input('cual es el color de su cabello?')
-if pregunta2 == 'negro':
+pregunta2 = input('cual es el color de su cabello?: ')
+if pregunta2.lower() == 'negro':
     puntaje = puntaje + 1
-pregunta3 = input('que tipo de cabello tiene?. Lacio, rizado o afro?')
-if pregunta3 == 'rizado':
+pregunta3 = input('que tipo de cabello tiene?. lacio, rizado o afro?: ')
+if pregunta3.lower() == 'rizado':
     puntaje = puntaje + 1
-pregunta4 = input('que shampoo usa?. Pantene, Head&Shoulders o Sedal')
-if pregunta4 == 'Sedal':
+pregunta4 = input('que shampoo usa?. pantene, head&shoulders o sedal: ')
+if pregunta4.lower() == 'sedal':
     puntaje = puntaje + 1
 
 if puntaje == 4:
