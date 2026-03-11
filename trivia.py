eaab5fdbb3881b9679ee1dd2ca7f85b55fcfc195
Samuel Karjallo <karjallo@disroot.org>
2026-03-11 17:12:30 -0300

    generamos las preguntas

--- trivia.py
+++ trivia.py
@@ -3,3 +3,9 @@ puntaje = 0
 
 print(f'bienvenido {nombre_usuario}')
 
+pregunta1 = input('cual es el nombre del coach de cabello largo?')
+pregunta2 = input('cual es el color de su cabello?')
+pregunta3 = input('que tipo de cabello tiene?')
+pregunta4 = input('que shampoo usa?. Pista: Pantene, Head&Shoulders o Sedal')
+
+
